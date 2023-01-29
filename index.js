8ad1c874553c7f516aacf0ea246468a3aaac33d0
Rishi0814 <rishibarad007@gmail.com>
2023-01-29 11:26:14 +0530

    updated project Structure

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { response } = require("express");
-
 const app = express();
 const port = process.env.PORT || 5000;
 const url = "https://www.theguardian.com/uk";
 const cors = require("cors");
 app.use(cors());
-
-// app.get("/",(req,res)=>{
-//     res.send("WEBSCRAPPER");
-// })
-// app.get("/results", (req, res) => {
-//   axios(url)
-//     .then((response) => {
-//       const html = response.data;
-//       const $ = cheerio.load(html);
-//       // console.log($)
-//       const articles = [];
-//       $("#myTopnav", html).each(function () {
-//         const title = $(this).text();
-//         const link = $(this).find("a").attr("href");
-//         articles.push(title, link);
-//       });
-//       // console.log(articles);
-//       res.send(articles);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       // res.send(err);
-//     });
-// });
 
 app.get("/results", (req, res) => {
   axios(url)
@@ -50,8 +24,6 @@ app.get("/results", (req, res) => {
       console.log(err);
     });
 });
-
-// app.use("/", require("./routes/router"));
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
